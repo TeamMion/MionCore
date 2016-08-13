e1b7b20576e154baa31fa4ee14938d91bbe3b1ad
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-08-13 07:02:20 +0200

    Changes in BlockContainer, Pre-release 0.3.0

--- src/main/java/teammion/mioncore/block/BlockContainer.java
+++ src/main/java/teammion/mioncore/block/BlockContainer.java
@@ -18,7 +18,9 @@ package teammion.mioncore.block;
 
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.EnumBlockRenderType;
 import teammion.mioncore.util.INameSetter;
 import teammion.mioncore.util.IStackable;
 
@@ -29,6 +31,8 @@ import teammion.mioncore.util.IStackable;
  */
 public abstract class BlockContainer extends net.minecraft.block.BlockContainer implements IStackable, INameSetter
 {
+    private EnumBlockRenderType blockRenderType = EnumBlockRenderType.MODEL;
+    
     public BlockContainer(String name, Material materialIn)
     {
         super(materialIn);
@@ -53,7 +57,18 @@ public abstract class BlockContainer extends net.minecraft.block.BlockContainer 
     public BlockContainer(String name, Material materialIn, MapColor color, CreativeTabs creativeTab)
     {
         this(name, materialIn, color);
-
+        
         setCreativeTab(creativeTab);
+    }
+    
+    public void setBlockRenderType(EnumBlockRenderType blockRenderType)
+    {
+        this.blockRenderType = blockRenderType;
+    }
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return blockRenderType;
     }
 }
