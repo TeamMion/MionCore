10411452d1cf1a1c2eded3cfaee454b71d57a8f9
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-10-31 20:18:32 +0100

    Changed BlockCrop to BlockPlant; Change for RichtClickHarvest

--- src/main/java/teammion/mioncore/block/BlockPlant.java
+++ src/main/java/teammion/mioncore/block/BlockPlant.java
@@ -16,13 +16,16 @@
 
 package teammion.mioncore.block;
 
+import net.minecraft.block.BlockCrops;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import teammion.mioncore.util.INameSetter;
 import teammion.mioncore.util.IStackable;
@@ -30,22 +33,22 @@ import teammion.mioncore.util.IStackable;
 import javax.annotation.Nullable;
 
 /**
- * BlockCrops base class
+ * BlockPlant base class
  *
  * @author Stefan Wimmer {@literal <stefanwimmer128@gmail.com>}
  */
-public class BlockCrops extends net.minecraft.block.BlockCrops implements IStackable, INameSetter
+public class BlockPlant extends BlockCrops implements IStackable, INameSetter
 {
     private Item seed;
     private Item crop;
     
     /**
-     * Creates {@link BlockCrops} with Registry/Unlocalized name, Seed Item and Crop Item
+     * Creates {@link BlockPlant} with Registry/Unlocalized name, Seed Item and Crop Item
      * @param name Registry/Unlocalized name
      * @param seed Seed Item
      * @param crop Crop Item
      */
-    public BlockCrops(String name, Item seed, Item crop)
+    public BlockPlant(String name, Item seed, Item crop)
     {
         this.seed = seed;
         this.crop = crop;
@@ -54,11 +57,11 @@ public class BlockCrops extends net.minecraft.block.BlockCrops implements IStack
     }
     
     /**
-     * Creates {@link BlockCrops} with Registry/Unlocalized name, Seed/Crop Item
+     * Creates {@link BlockPlant} with Registry/Unlocalized name, Seed/Crop Item
      * @param name Registry/Unlocalized name
      * @param both Seed/Crop Item
      */
-    public BlockCrops(String name, Item both)
+    public BlockPlant(String name, Item both)
     {
         this(name, both, both);
     }
@@ -66,13 +69,13 @@ public class BlockCrops extends net.minecraft.block.BlockCrops implements IStack
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
     {
-        if (allowRightClickHarvest() && isMaxAge(state))
+        if (enableRightClickHarvest() >= 0 && isMaxAge(state))
         {
             for (ItemStack stack : getDrops(worldIn, pos, state, 0))
             {
                 spawnAsEntity(worldIn, pos, stack);
                 
-                worldIn.setBlockState(pos, withAge(getRightClickHarvestResetState()));
+                worldIn.setBlockState(pos, withAge(enableRightClickHarvest()));
             }
             
             return true;
@@ -93,22 +96,18 @@ public class BlockCrops extends net.minecraft.block.BlockCrops implements IStack
         return crop;
     }
     
-    /**
-     * Returns {@code true} when harvest by right clicking is allowed
-     * {@link BlockCropsRightClickHarvest} has this implemented already
-     * @return Harvest by right click is allowed
-     */
-    public boolean allowRightClickHarvest()
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
     {
-        return false;
+        return new AxisAlignedBB(0, 0, 0, 1, 1, 1);
     }
     
     /**
-     * Returns {@code int} of state to reset after harvesting with right click
-     * @return State to reset to after harvesting with right click
+     * Returns {@code -1} when harvest by right clicking is not allowed
+     * @return Age to be set to after rich click harvest
      */
-    public int getRightClickHarvestResetState()
+    public int enableRightClickHarvest()
     {
-        return 0;
+        return -1;
     }
 }
